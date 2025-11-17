9a48ef95ce535e71ca7c4aa23e98f39b1ae37de3
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-11-17 17:52:00 +0100

    Created a generator to compute a memory-efficient aggregate function i.e average age for a large dataset

---
+++ python-generators-0x00/4-stream_ages.py
@@ -0,0 +1,39 @@
+from logger import logger
+from seed import connect_db
+
+connection = connect_db(database="ALX_prodev")
+
+def stream_user_ages():
+    """Generator that yields user ages from the database."""
+    if not connection:
+        logger.error("Failed to connect to the database for streaming ages.")
+        return
+    
+    cursor = connection.cursor(dictionary=True)
+    cursor.execute("SELECT age FROM user_data;")
+    for row in cursor:
+        logger.info(f"Yielding age: {row['age']}")
+        yield row['age']
+    
+    cursor.close()
+    connection.close()
+
+def calculate_average_age():
+    """Calculate the average age of users in the database."""
+    total_age = 0
+    number_of_users = 0
+    for age in stream_user_ages():
+        total_age += age
+        number_of_users += 1
+    
+    average_age = total_age / number_of_users if number_of_users > 0 else 0
+    return average_age
+
+def main():
+    logger.info("Starting to calculate average age of users...")
+    average_age = calculate_average_age()
+    logger.info(f"Average age of users: {average_age}")
+
+
+if __name__ == "__main__":
+    main()
--- python-generators-0x00/main.py
+++ python-generators-0x00/main.py
@@ -4,3 +4,4 @@ import subprocess
 subprocess.run(["python", "0-stream_users.py"])
 subprocess.run(["python", "1-batch_processing.py"])
 subprocess.run(["python", "2-lazy_paginate.py"])
+subprocess.run(["python", "4-stream_ages.py"])
