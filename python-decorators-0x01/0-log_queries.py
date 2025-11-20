7a3c0a30153ea0013dc7753d21deee166ccfefce
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-11-20 21:56:57 +0100

    A decorator that logs database queries executed by any function

---
+++ python-decorators-0x01/0-log_queries.py
@@ -0,0 +1,63 @@
+import os
+import time
+import functools
+import mysql.connector
+from logger import logger
+from dotenv import load_dotenv
+
+
+def connect_db(retries=10, delay=5, database=None):
+    for attempt in range(retries):
+        try:
+            logger.info("Connecting to the MySQL database...")
+
+            conn_params = {
+                "host": "db",
+                "user": os.getenv("MYSQL_USER"),
+                "password": os.getenv("MYSQL_PASSWORD"),
+                "port": 3306
+            }
+            if database:
+                conn_params["database"] = database
+
+            connection = mysql.connector.connect(**conn_params)
+
+            logger.info(
+                "Successfully connected to MySQL"
+                + (f" database '{database}'." if database else " (no default database).")
+            )
+            return connection
+
+        except mysql.connector.Error as err:
+            logger.error(f"Error connecting to the database: {err}")
+            time.sleep(delay)
+
+        finally:
+            logger.info("Database connection attempt finished.")
+    return None
+
+def log_queries(func):
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs):
+        query = kwargs.get("query")
+        if query is None and len(args) > 0:
+            query = args[0]
+        result = func(*args, **kwargs)
+        print(f"[LOG] Executing SQL Query: {query}")
+        return result
+    return wrapper
+
+@log_queries
+def fetch_all_users(query):
+    db_connection = connect_db(database="ALX_prodev")
+    cursor = db_connection.cursor()
+    cursor.execute(query)
+    results = cursor.fetchall()
+    db_connection.close
+    return results
+
+
+if __name__ == "__main__":
+    users = fetch_all_users(query="SELECT * FROM user_data;")
+    logger.info(f"Users: ", users)
+
