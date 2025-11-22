3c49c14d79b900dcdc3d9cf656a759b5ef6e8058
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-11-22 18:53:05 +0100

    cleanup - removed comments

--- python-generators-0x00/main.py
+++ python-generators-0x00/main.py
@@ -1,10 +1,10 @@
 import subprocess
 
 
-# subprocess.run(["python", "0-stream_users.py"])
-# subprocess.run(["python", "1-batch_processing.py"])
-# subprocess.run(["python", "2-lazy_paginate.py"])
-# subprocess.run(["python", "4-stream_ages.py"])
-# subprocess.run(["python", "../python-decorators-0x01/0-log_queries.py"])
-# subprocess.run(["python", "../python-decorators-0x01/1-with_db_connection.py"])
+subprocess.run(["python", "0-stream_users.py"])
+subprocess.run(["python", "1-batch_processing.py"])
+subprocess.run(["python", "2-lazy_paginate.py"])
+subprocess.run(["python", "4-stream_ages.py"])
+subprocess.run(["python", "../python-decorators-0x01/0-log_queries.py"])
+subprocess.run(["python", "../python-decorators-0x01/1-with_db_connection.py"])
 subprocess.run(["python", "../python-decorators-0x01/2-transactional.py"])
