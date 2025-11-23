2bcf6d72e0051722cdc6032afbc9a79f47139d88
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-11-23 20:18:40 +0100

    created a users table and seeded this table

--- python-context-async-perations-0x02/0-databaseconnection.py
+++ python-context-async-perations-0x02/0-databaseconnection.py
@@ -9,6 +9,10 @@ class DatabaseConnection:
     Opens the connection in __enter__ and closes it in __exit__.
     """
 
+    def __init__(self, database="ALX_prodev"):
+        self.database = database
+        self.conn = None
+
     def __enter__(self):
         self.conn = mysql.connector.connect(
             host="db",
@@ -23,11 +27,34 @@ class DatabaseConnection:
         if self.conn and self.conn.is_connected():
             self.conn.close()
         
+create_table_sql = """
+    CREATE TABLE IF NOT EXISTS users (
+        user_id VARCHAR(36) PRIMARY KEY,
+        name VARCHAR(100),
+        email VARCHAR(100),
+        age INT
+    );
+"""
+
+insert_records_sql = """
+    INSERT INTO users (user_id, name, email, age) VALUES (%s, %s, %s, %s)
+"""
+
+records = [
+    ("00369a24-c017-4ed4-ac4d-31fc80f9a6ae", "Dustin Mayer",      "chidienew@ceot.com",             56),
+    ("004e5d01-fd17-44ab-a1aa-d8c528c3ba01", "Robin Wilkinson",   "Brent_Wilkinson2@hotmail.com",   62),
+    ("005e01c3-40c6-433f-a85d-595f51633cca", "Elbert Gutmann",    "Julius15@hotmail.com",           33),
+    ("00baf98d-3823-4a85-b613-683191bb20b1", "Dr. Amos Swift Sr.", "Teri67@hotmail.com",             7),
+    ("019fb8fa-deb6-4e48-8f07-63b93b3c8454", "Kim Pfeffer",       "Camille.Stanton48@yahoo.com",    None),
+]
 
 with DatabaseConnection() as conn:
     cursor = conn.cursor(dictionary=True)
-    cursor.execute("SELECT * FROM user_data LIMIT 10;")
+    cursor.execute(create_table_sql)
+    cursor.executemany(insert_records_sql, records)
+
+    cursor.execute("SELECT * FROM users LIMIT 3;")
     results = cursor.fetchall()
     cursor.close()
 
-    logger.info(f"Database Query Results: {results}")
+    logger.info(f"Database Query Results For Users: {results}")
