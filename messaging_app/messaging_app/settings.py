9805f04d39ca88b2756dfa688f4ebe52faf09709
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-12-06 12:06:51 +0100

    changed rest_framework permission

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -61,7 +61,7 @@ MIDDLEWARE = [
 # REST Framework configuration
 REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.AllowAny",
+        "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
         "rest_framework.authentication.SessionAuthentication",
