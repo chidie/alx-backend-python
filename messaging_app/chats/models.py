699b363c203e309837f53503571959e3d1676a3d
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-12-18 14:48:03 +0100

    changed user_id to id to avoid token issues

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import AbstractUser
 from django.core.exceptions import ValidationError
 
 class User(AbstractUser):
-    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=150, null=False, blank=False)
     last_name = models.CharField(max_length=150, null=False, blank=False)
     email = models.EmailField(unique=True, null=False, blank=False)
