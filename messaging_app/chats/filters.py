3b406b4e8aaecdb0c1c5a17cd7a06cc7b96ea883
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-12-26 20:45:05 +0100

    updated the dockerfile

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,7 +1,7 @@
 import django_filters
 from .models import Conversation, Message
 
-class MessageFilter(django.FilterSet):
+class MessageFilter(django_filters.FilterSet):
     sender = django_filters.CharFilter(field_name="sender__email", lookup_expr='iexact')
     start_time = django_filters.DateTimeFilter(field_name="created_at", lookup_expr='gte')
     end_time = django_filters.DateTimeFilter(field_name="created_at", lookup_expr='lte')
