665b90bf78d7a0dcf5e03ad9470db73b662d5ed7
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-12-30 02:11:01 +0100

    implement a middleware that restricts access to the messaging

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,7 +1,8 @@
 import os
 import logging
-from datetime import datetime
+from datetime import datetime, time
 from django.conf import settings
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     """
@@ -24,5 +25,28 @@ class RequestLoggingMiddleware:
         log_entry = f"{datetime.now()} - User: {user} - Path: {request.path}"
         logging.info(log_entry)
 
+        response = self.get_response(request)
+        return response
+
+class RestrictAccessByTimeMiddleware:
+    """
+    Middleware to restrict access to certain views based on time of day.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        # self.restricted_start = time(22, 0)  # 10 PM
+        # self.restricted_end = time(6, 0)     # 6 AM
+
+    def __call__(self, request):
+        # Define restricted hours: outside 6AM–9PM
+        start_allowed = time(6, 0)
+        end_allowed = time(21, 0)
+        current_time = datetime.now().time()
+
+        # If current_time is outside allowed range, block access
+        if not (start_allowed <= current_time <= end_allowed):
+            return HttpResponseForbidden("Access to this resource is restricted during this time.")
+        
         response = self.get_response(request)
         return response
