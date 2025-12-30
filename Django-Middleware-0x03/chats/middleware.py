c6555e5ce1af5dd7f9d347a6a2e6bb6a23321a1f
Chidiebere Onuoha <chidieonuoha@gmail.com>
2025-12-30 02:39:00 +0100

    Implement middleware that limits the number of chat messages a user can send within a certain time window, based on their IP address.

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from datetime import datetime, time
+from datetime import datetime, time, timedelta
 from django.conf import settings
 from django.http import HttpResponseForbidden
 
@@ -50,3 +50,39 @@ class RestrictAccessByTimeMiddleware:
         
         response = self.get_response(request)
         return response
+
+class OffensiveLanguageMiddleware:
+    """
+    Middleware to filter offensive language in messages.
+    """
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+        self.ip_request_log = {} # Store request timestamps per IP
+        self.REQUEST_LIMIT = 5  # Max requests
+        self.TIME_WINDOW = timedelta(minutes=1)  # Time window
+
+    def __call__(self, request):
+        # Only rate-limit POST requests (messages)
+        if request.method == "POST":
+            ip = self.get_client_ip(request)
+            now = datetime.now()
+
+            # Initialize list for this IP
+            if ip not in self.ip_request_log:
+                self.ip_request_log[ip] = []
+            
+            # Remove timestamps older than 1 minute
+            self.ip_request_log[ip] = [
+                ts for ts in self.ip_request_log[ip] if now - ts < self.TIME_WINDOW
+            ]
+
+            self.ip_request_log[ip].append(now)
+        
+        return self.get_response(request)
+
+    def get_client_ip(self, request):
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            return x_forwarded_for.split(',')[0]
+        return request.META.get('REMOTE_ADDR')
--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -87,6 +87,7 @@ MIDDLEWARE = [
     # Custom Middleware
     'chats.middleware.RequestLoggingMiddleware',
     'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
 ]
 
 ROOT_URLCONF = 'Django-Middleware-0x03.urls'
